789639c4ed86d92457c6f2d3c3e71a239c1f4997
Kendy Augustin <Kendya44@gmail.com>
2016-06-20 23:47:39 -0500

    Update poll-bot-worker.php

--- poll-bot-worker.php
+++ poll-bot-worker.php
@@ -4,7 +4,7 @@ set_time_limit(0);
 
 require_once 'PollBot.php';
 
-define('BOT_TOKEN', 'XXXXXXXXX:XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX');
+define('BOT_TOKEN', '152410440:AAGOEVJaI8GF98Zcmln6a310ivnLmxxawLE');
 
 $bot = new PollBot(BOT_TOKEN, 'PollBotChat');
 $bot->runLongpoll();
